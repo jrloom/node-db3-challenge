cc95b963e94175e0eafe17c23c79658e95b23480
Jason Loomis <jason@jrloomis.com>
2019-11-06 16:35:51 -0500

    added sanity check, changed port

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const SchemeRouter = require('./schemes/scheme-router.js');
+const SchemeRouter = require("./schemes/scheme-router.js");
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+server.use("/api/schemes", SchemeRouter);
+
+// * sanity
+server.get("/", (req, res) => res.status(200).json({ message: "asffwe" }));
 
 module.exports = server;
